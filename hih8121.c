804a72ac15dc705fdf64959c8a3293f71e5658fe
Alex Avery <aja9675@rit.edu>
2019-09-12 17:29:49 -0400

    Add temp C and a combined temp/humidity node for efficiency
    Signed-off-by: Alex Avery <aja9675@rit.edu>

--- hih8121.c
+++ hih8121.c
@@ -23,9 +23,10 @@ static const struct i2c_device_id hih8121_i2c_id[] = {
 MODULE_DEVICE_TABLE(i2c, hih8121_i2c_id);
 
 
-static int hih8121_read_raw_data(struct device *dev, u8 *data) {
+static int hih8121_read_data(struct device *dev, int *temp_f, int *temp_c, int *humidity) {
     struct i2c_client *client = to_i2c_client(dev);
-    int ret;
+    int ret, temp_raw;
+    u8 data[4];
 
         /* Hacked 0 byte write to trigger the conversion since our smbus implementation
      * doesn't support smbus_quick_write() */
@@ -46,52 +47,51 @@ static int hih8121_read_raw_data(struct device *dev, u8 *data) {
 	    ret = -EIO;
     }
 
+   	temp_raw = (((data[2] & 0xFF) * 256) + (data[3] & 0xFC)) / 4;
+   	// Scaling up to report decidegrees without floating point math
+    *temp_c = ((temp_raw * 1650) >> 14) - 400; // (temp_raw / 16384.0) * 165.0 - 40.0;
+    *temp_f = *temp_c * 18 / 10 + 320; // cTemp * 1.8 + 32
+
+    // humidity = ((((data[0] & 0x3F) * 256) + data[1]) * 100.0) / 16383.0
+    *humidity = ((((data[0] & 0x3F) * 256) + data[1]) * 1000) >> 14;
+
 	return ret;
 }
 
-static ssize_t read_temperature_f(struct device *dev, 
+/*
+ * I think there's a better way to do this at compile time.. But this works for now.
+ */
+static ssize_t read_generic_temp_humid(struct device *dev,
     struct device_attribute *dev_attr,
     char *buf)
 {
-    int ret, temp_raw, temp_c, temp_f;
-    u8 data[4];
+    int ret, temp_c, temp_f, humidity;
+    const char * name = dev_attr->attr.name;
 
-    ret = hih8121_read_raw_data(dev, data);
+    ret = hih8121_read_data(dev, &temp_f, &temp_c, &humidity);
     if (ret < 0)
     	return ret;
    	
-   	temp_raw = (((data[2] & 0xFF) * 256) + (data[3] & 0xFC)) / 4;
-   	// Scaling up to report decidegrees without floating point math
-    temp_c = ((temp_raw * 1650) >> 14) - 400; // (temp_raw / 16384.0) * 165.0 - 40.0;
-    temp_f = temp_c * 18 / 10 + 320; // cTemp * 1.8 + 32
-
-    return sprintf(buf, "%d\n", temp_f);
+    if (strncmp(name, "read_temp_f", PAGE_SIZE) == 0) {
+		return sprintf(buf, "%d\n", temp_f);
+    } else if (strncmp(name, "read_temp_c", PAGE_SIZE) == 0) {
+		return sprintf(buf, "%d\n", temp_c);
+    } else if (strncmp(name, "read_humid", PAGE_SIZE) == 0) {
+		return sprintf(buf, "%d\n", humidity);
+	}
+    return sprintf(buf, "%d %d\n", temp_f, humidity);
 }
 
-static ssize_t read_humidity(struct device *dev, 
-    struct device_attribute *dev_attr,
-    char *buf)
-{
-    int ret, humidity;
-    u8 data[4];
-
-    ret = hih8121_read_raw_data(dev, data);
-    if (ret < 0)
-    	return ret;
-   	
-    //humidity = ((((data[0] & 0x3F) * 256) + data[1]) * 100.0) / 16383.0
-    humidity = ((((data[0] & 0x3F) * 256) + data[1]) * 1000) >> 14;
-
-    return sprintf(buf, "%d\n", humidity);
-}
-
-
-static DEVICE_ATTR(read_temp_f, S_IRUGO, read_temperature_f, NULL);
-static DEVICE_ATTR(read_humid, S_IRUGO, read_humidity, NULL);
+static DEVICE_ATTR(read_temp_f, S_IRUGO, read_generic_temp_humid, NULL);
+static DEVICE_ATTR(read_temp_c, S_IRUGO, read_generic_temp_humid, NULL);
+static DEVICE_ATTR(read_humid,  S_IRUGO, read_generic_temp_humid, NULL);
+static DEVICE_ATTR(read_temp_humid, S_IRUGO, read_generic_temp_humid, NULL);
 
 static struct attribute *hih8121_attrs[] = {
 	&dev_attr_read_temp_f.attr,
+	&dev_attr_read_temp_c.attr,
 	&dev_attr_read_humid.attr,
+	&dev_attr_read_temp_humid.attr,
 	NULL
 };
 
